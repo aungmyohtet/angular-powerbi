8e8aa21e6daab66a1b7c948c51e1cddeecbdbc93
Aung Myo Htet <aungmyohtet@outlook.com>
2023-09-08 10:07:47 +0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { PowerBIEmbedModule } from "powerbi-client-angular";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, PowerBIEmbedModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
